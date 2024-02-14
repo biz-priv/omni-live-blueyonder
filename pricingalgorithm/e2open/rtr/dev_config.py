3bd9bc42c1b75e491bc19efe6cd84f964af4cc69
raviteja <raviteja.kalluri@bizcloudexperts.com>
2024-02-14 23:38:54 +0530

    cleaned up code

--- pricingalgorithm/e2open/rtr/dev_config.py
+++
@@ -1,5 +0,0 @@
-e2open_notification = 'e2open_email.html'
-from_address = 'noreply@omnilogistics.com'
-to_ken_address = ['drew@sightlinefreight.com']
-to_nutra_address = ['drew@sightlinefreight.com']
-to_matt_address= ['drew@sightlinefreight.com']
--- pricingalgorithm/formswebhook/index.py
+++
@@ -1,6 +0,0 @@
-from formstack_webhook_parsing import get_data
-
-def lambda_handler(event, context):
-    print(event)
-    data = event['data']
-    return get_data(data)
